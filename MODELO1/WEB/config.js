12c84737ca6b618e84d2cd324a5343a97fc72e8c
Seu Nome <arthurdarkaibo@gmail.com>
2025-07-02 05:18:40 -0300

    feat: estrutura inicial com MODELO1/BOT e MODELO1/WEB

---
+++ MODELO1/WEB/config.js
@@ -0,0 +1,8 @@
+window.config = {
+  title: "Conteúdo Proibido 🔞",
+  headline: "Conteúdo quente liberado por tempo limitado! 🔥",
+  description: "Essa gostosa separou o pack completo só pros mais ousados... Você tá dentro?",
+  buttonText: "Quero ver tudo agora",
+  redirectLink: "https://t.me/vipsbruninha_bot",
+  backgroundImage: "assets/imagem.jpg"
+}
