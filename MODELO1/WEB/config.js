8e911edc0d456aaff8a5b657db160e72e664801e
patolina1000 <arthurdarkaibo@gmail.com>
2025-07-08 09:41:37 -0300

    Update redirect link

--- MODELO1/WEB/config.js
+++ MODELO1/WEB/config.js
@@ -3,6 +3,6 @@ window.config = {
   headline: "O quartinho da sobrinha tá aberto, titio... mas não por muito tempo. 💗",
   description: "Tem umas coisinhas bem íntimas te esperando... Vai entrar ou vai ficar babando do lado de fora?",
   buttonText: "Antes que feche esse quartinho...",
-  redirectLink: "https://t.me/vipsbruninha_bot",
+  redirectLink: "https://t.me/vipshadrie_bot",
   backgroundImage: "assets/imagem.jpg"
 };
