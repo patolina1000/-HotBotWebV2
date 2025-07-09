529c8338b1239c1d4741a3fce2a02c2d4318c477
patolina1000 <arthurdarkaibo@gmail.com>
2025-07-09 03:28:07 -0300

    Update welcome page layout and text

--- MODELO1/WEB/config.js
+++ MODELO1/WEB/config.js
@@ -1,8 +1,8 @@
 window.config = {
   title: "Tem uma parte minha que você ainda não viu...",
   headline: "Tem uma parte minha que você ainda não viu...",
-  description: "Eu gravei tudo. Acompanhada. Entrega real, gemido real... e só pra quem tem coragem de ir até o fim.",
-  buttonText: "Acessar agora",
+  description: "Eu gravei tudo. Acompanhada.\nEntrega real, gemido real... e só pra quem tem coragem de ir até o fim.",
+  buttonText: "Quero ver agora, sem censura",
   redirectLink: "https://t.me/vipsbruninha_bot",
   backgroundImage: "assets/imagem.jpg"
 };
