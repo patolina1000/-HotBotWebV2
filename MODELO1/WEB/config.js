bc24c3e339e8df9bc88bf4350d775e4cb739a9ae
patolina1000 <arthurdarkaibo@gmail.com>
2025-07-09 10:05:08 -0300

    Atualiza link de redirecionamento do CTA

--- MODELO1/WEB/config.js
+++ MODELO1/WEB/config.js
@@ -3,6 +3,6 @@ window.config = {
   headline: "Tem uma parte minha que você ainda não viu...",
   description: "Eu gravei tudo. Acompanhada.\nEntrega real, gemido real... e só pra quem tem coragem de ir até o fim.",
   buttonText: "Quero ver agora, sem censura",
-  redirectLink: "https://t.me/vipsbruninha_bot",
+  redirectLink: "https://t.me/vipshadrie_bot",
   backgroundImage: "assets/imagem.jpg"
 };
